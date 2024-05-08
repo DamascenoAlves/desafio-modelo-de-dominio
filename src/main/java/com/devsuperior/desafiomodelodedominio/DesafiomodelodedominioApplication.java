2d47ea6c923f916056fd21a6c1f6297b4eb5f51f
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-05-08 17:05:45 -0300

    First commit

---
+++ src/main/java/com/devsuperior/desafiomodelodedominio/DesafiomodelodedominioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodelodedominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiomodelodedominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiomodelodedominioApplication.class, args);
+	}
+
+}
